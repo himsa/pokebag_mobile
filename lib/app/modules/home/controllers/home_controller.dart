54b974fc592deabb0b081be57d309f054aa4e529
Himsa Yudhistira <himsas53@gmail.com>
2022-04-04 15:01:16 +0700

    add dark mode feature

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pokebag_mobile/core/local/get_storage.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  final isDark = Get.isPlatformDarkMode.obs;
+  final initialIcon = 0.obs;
 
-  final count = 0.obs;
   @override
   void onInit() {
+    isDark.value =
+        GetStorageManager.getStorage.read(GetStorageManager.darkmode) ??
+            Get.isPlatformDarkMode;
+    if (isDark.value) {
+      initialIcon.value = 0;
+    } else {
+      initialIcon.value = 1;
+    }
+
     super.onInit();
   }
 
   @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
   void onClose() {}
-  void increment() => count.value++;
+
+  void changeTheme(bool val) {
+    Get.changeThemeMode(val ? ThemeMode.dark : ThemeMode.light);
+    GetStorageManager.getStorage.write(GetStorageManager.darkmode, val);
+    isDark.value = val;
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:pokebag_mobile/core/widgets/home_app_bar.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          child: Obx(
+            () => HomeAppBar(
+              onPressedDarkMode: () =>
+                  controller.changeTheme(!controller.isDark.value),
+              initialIConIndex: controller.initialIcon.value,
+            ),
+          ),
+          preferredSize: Size.fromHeight(kToolbarHeight),
+        ),
+        body: Center(
+          child: Text(
+            'HomeView is working',
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ),
     );
